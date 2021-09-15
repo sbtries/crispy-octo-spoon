2f283b89f4375c0ebc2d3290eef931aca958d70c
sbtries <sarahbtracy17@gmail.com>
2021-09-15 12:51:16 -0400

    I messed up my model and migrations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,3 +2,14 @@ class ArticlesController < ApplicationController
   def index
   end
 end
+
+# When an action does not explicitly render a view 
+# (or otherwise trigger an HTTP response), 
+# Rails will automatically render a view that matches 
+# the name of the controller and action. 
+# "Convention Over Configuration! "
+
+
+# Views are located in the app/views directory.
+#  So the index action will render app/views/articles/index.html.erb 
+#  by default.
--- db/migrate/20210915143117_create_articles.rb
+++ db/migrate/20210915143117_create_articles.rb
@@ -1,10 +1,12 @@
-class CreateArticles < ActiveRecord::Migration[6.1]
+class CreateArticles < ActiveRecord::Migration[6.0]
   def change
-    create_table :articles do |t|
-      t.string :title
-      t.text :body
-
-      t.timestamps
+    create_table :articles do |t| #specifies how the articles table should be constructed.
+                                  #by defauly, create_table method adds an id column as an 
+                                  #incrementing pk 
+      t.string :title #col -> added because they were includewd in the command
+      t.text :body #col 
+      t.timestamps #defintes 2 more columns, created_at and updated_at. 
+                  #Rails manages these & sets values when objects are created or updated
     end
   end
 end
