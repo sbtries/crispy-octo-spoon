6e2c7fa6a3cc65e1089f2a7b5b16cbc690480835
sbtries <sarahbtracy17@gmail.com>
2021-09-16 08:39:04 -0400

    adding create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,24 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id]) #instance variable, accessible by view
     # *** By DEFAULT, the show action will render app/views/articles/show.html.erb.
   end
+
+  def new # instantiates a new article, but does not save it.
+    @article = Article.new
+    # *** by DEFAULT new action will render app/views/articles/new.html.erb
+  end
+
+  def create # instantiates a new article with values for the title and body, and attempts to save it
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      # if successful, redicts to article's page ("http://localhost:3000/articles/#{@article.id}")
+      redirect_to @article
+    else
+      #redisplays the form by rendering app/views/articles/new.html.erb
+      render :new
+    end
+
+
 end
 
 # When an action does not explicitly render a view 
